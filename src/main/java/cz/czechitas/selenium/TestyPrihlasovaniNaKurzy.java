5fadf14d73448f087917205ca765c549eecec237
Petra Antlová <petraantlova@gmail.com>
2021-05-13 00:21:17 +0200

    Pridani druheho testu

--- src/main/java/cz/czechitas/selenium/TestyPrihlasovaniNaKurzy.java
+++ src/main/java/cz/czechitas/selenium/TestyPrihlasovaniNaKurzy.java
@@ -36,7 +36,42 @@ public class TestyPrihlasovaniNaKurzy {
     }
 
     @Test
+    public void prihlaseniDiteteDoKurzu() {
+        prohlizec.navigate().to("https://cz-test-jedna.herokuapp.com/11-trimesicni-kurzy-webu");
+        WebElement tlacitkoVytvoritPrihlasku = prohlizec.findElement(By.xpath("/html/body/div/div/div/div/div[2]/div/div/div[2]/a"));
+        tlacitkoVytvoritPrihlasku.click();
+        WebElement polickoEmail = prohlizec.findElement(By.id("email"));
+        polickoEmail.sendKeys("panitestova@gmail.com");
+        WebElement polickoHeslo = prohlizec.findElement(By.id("password"));
+        polickoHeslo.sendKeys("DAczechitas2021");
+        WebElement mensiTlacitkoPrihlasit = prohlizec.findElement(By.xpath("/html/body/div/div/div/div/div/div/form/div[3]/div/button"));
+        mensiTlacitkoPrihlasit.click();
+        //WebElement polickoTermin = prohlizec.findElement(By.id("/html/body/div/div/div/div/div/form/table/tbody/tr[2]/td[2]/div/button/div/div/div"));
+        //polickoTermin.click();
+        //WebElement polickoVyhledaniTerminu = prohlizec.findElement(By.xpath("/html/body/div/div/div/div/div/form/table/tbody/tr[2]/td[2]/div/div/div[1]/input"));
+        //polickoVyhledaniTerminu.click();
+        //polickoVyhledaniTerminu.sendKeys("2");
+        //polickoVyhledaniTerminu.sendKeys("\n");
+        WebElement polickoJmenoZaka = prohlizec.findElement(By.id("forename"));
+        polickoJmenoZaka.sendKeys("Manual");
+        WebElement polickoPrijmeniZaka = prohlizec.findElement(By.id("surname"));
+        polickoPrijmeniZaka.sendKeys("Test");
+        WebElement polickoDatumNarozeniZaka = prohlizec.findElement(By.id("birthday"));
+        polickoDatumNarozeniZaka.sendKeys("12.05.2012");
+        WebElement zaskrtavaciPolickoPlatbaHotove = prohlizec.findElement(By.xpath("/html/body/div/div/div/div/div/form/table/tbody/tr[8]/td[2]/span[4]/label"));
+        zaskrtavaciPolickoPlatbaHotove.click();
+        WebElement zaskrtavaciPolickoSouhlasSPodminkami = prohlizec.findElement(By.xpath("/html/body/div/div/div/div/div/form/table/tbody/tr[11]/td[2]/span/label"));
+        zaskrtavaciPolickoSouhlasSPodminkami.click();
+        //WebElement tlacitkoVytvoritPrihlaskuNaKurz = prohlizec.findElement(By.xpath("/html/body/div/div/div/div/div/form/table/tbody/tr[11]/td[2]/input"));
+        //tlacitkoVytvoritPrihlaskuNaKurz.click();
+        //WebElement zalozkaPrihlasky = prohlizec.findElement(By.xpath("/html/body/div/header/div/nav/span[2]/a"));
+        //zalozkaPrihlasky.click();
 
+        //List<WebElement> seznamPrihlasek = prohlizec.findElements(By.xpath("//*[@id=\"DataTables_Table_0\"]/tbody/tr/td[1]"));
+        //WebElement elementPrvniPrihlaska = seznamPrihlasek.get(0);
+        //String prvniPrihlaska = elementPrvniPrihlaska.getText();
+        //Assertions.assertEquals("Automat Test", prvniPrihlaska);
+    }
 
     /*@Test
     public void zviratVTabulceMusiByt7() {
@@ -53,12 +88,8 @@ public class TestyPrihlasovaniNaKurzy {
         prohlizec.navigate().to("https://automation3.shinekamil.repl.co/");
         WebElement zalozkaTabulka = prohlizec.findElement(By.linkText("Table"));
         zalozkaTabulka.click();
+    */
 
-        List<WebElement> seznamZvirat = prohlizec.findElements(By.xpath("//table/tbody/tr/td[1]"));
-        WebElement elementPrvnihoZvirete = seznamZvirat.get(0);
-        String prvniZvire = elementPrvnihoZvirete.getText();
-        Assertions.assertEquals("Kočka", prvniZvire);
-    }*/
 
     @AfterEach
     public void tearDown() {
